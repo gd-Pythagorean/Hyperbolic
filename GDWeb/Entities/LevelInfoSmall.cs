e1c9b9b2c248e24d4f8abcda66538f870a091ce3
Noah <altenyt@gmail.com>
2020-06-25 22:00:20 -0400

    Add level card

---
+++ GDWeb/Entities/LevelInfoSmall.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GDWeb.Entities
+{
+    public class LevelInfoSmall
+    {
+        public string BackgroundURL { get; set; }
+
+        public TimeSpan Length { get; set; }
+
+        public string Title { get; set; }
+
+        //TODO: Replace this with a class instead.
+        public string Author { get; set; }
+
+        //TODO: Replace this with a class instead.
+        public string Song { get; set; }
+
+        public int Ratings { get; set; }
+
+        public int Downloads { get; set; }
+    }
+}
