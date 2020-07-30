2df4a3eb0a15592a769dd17babc8fb089ceb3a72
Noah <altenyt@gmail.com>
2020-07-30 04:59:27 -0400

    Add dark mode

---
+++ Hyperbolic/wwwroot/JS/Site.js
@@ -0,0 +1,17 @@
+﻿function toggleDarkTheme()
+{
+    let element = document.body;
+
+    if (element.style.backgroundColor == null || element.style.backgroundColor === "white")
+    {
+        element.style.backgroundColor = "#262626";
+        element.classList.add("dark-theme");
+    }
+    else
+    {
+        element.style.backgroundColor = "white";
+        element.classList.remove("dark-theme");
+    }
+
+    toggled = !toggled;
+}
